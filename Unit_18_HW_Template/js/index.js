60ee045a12de884c36c4e212c3f23c20a562d5c0
Sheza9 <78698112+Sheza9@users.noreply.github.com>
2021-07-21 12:45:05 -0400

    added effects/animation
    added typewriter effect top headings
    fixed download button issue
    added move animation to 3 images and its relative infomration

--- Unit_18_HW_Template/js/index.js
+++ Unit_18_HW_Template/js/index.js
@@ -1,11 +1,13 @@
-console.log("your index.js file is loaded correctly!");
+
+
+  console.log("your index.js file is loaded correctly!");
 $(document).ready(function(){
 $("button").on({
     mouseenter: function(){
       $(this).css("background-color", "lightgray");
     },  
     mouseleave: function(){
-      $(this).css("background-color", "lightblue");
+      $(this).css("background-color", "#303031");
     }, 
     click: function(){
       $(this).css("background-color", "red");
@@ -14,11 +16,31 @@ $("button").on({
 });
 
 
-$(document).ready(function(){
-    $(".parent").click(function(){
-      $(".child").slideDown("slow");
+
+$("button").click(function(){
+  $("div").animate({
+    left: '250px',
+    opacity: '0.5',
+    height: '150px',
+    width: '150px'
+  });
+}); 
+
    
 
 
-  
- 
+$(document).ready(function() {
+  $(".move1").animate({left: "+=100"}, 2000);
+  $(".move1").animate({left: "-=100"}, 1000);
+});
+
+$(document) .ready(function() {
+  $(".move2").animate({left: "+=1000"}, 0000);
+  $(".move2").animate({left: "-=1000"}, 2100);
+});
+
+
+
+
+
+
